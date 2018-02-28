16d320beb8b1b4c4af5146477b1a5d05df701683
pcg15 <pcg15@duke.edu>
2018-02-28 14:48:35 -0500

    fixes issues with pep8 styling in server.py

--- server.py
+++ server.py
@@ -3,7 +3,10 @@ def main():
     import requests
     r1 = requests.get("http://bme590.suyash.io/list")
     print(r1)
-    r2 = requests.post("http://bme590.suyash.io/student", json={"first_name": "Chace", "last_name": "Gore", "netid": "pcg15", "github_username": "pcg15", "team_name": "CPS"})
+    r2 = requests.post("http://bme590.suyash.io/student",
+                       json={"first_name": "Chace", "last_name": "Gore",
+                             "netid": "pcg15", "github_username": "pcg15",
+                             "team_name": "CPS"})
     r3 = requests.post("http://bme590.suyash.io/sum", json={"a": 1, "b": 2})
     sum_result = r3.json()
     print("The response was {0}.".format(sum_result['result']))
